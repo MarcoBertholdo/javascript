cde386192ce39ed5f48aaad170e543cf050b4ef4
Marco Bertholdo <marquinhus_120@hotmail.com>
2023-12-26 05:16:53 -0300

    att

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -1,2 +1,7 @@
-var vel = 60.5
+var vel = 49
 console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro')
+} else {
+    console.log('Você é brasileiro')
+}
