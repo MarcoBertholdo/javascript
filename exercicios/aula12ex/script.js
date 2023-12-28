619c39ba7043906086d6e1e2b5814b0b012bbda7
Marco Bertholdo <marquinhus_120@hotmail.com>
2023-12-28 12:43:33 -0300

    att

--- exercicios/aula12ex/script.js
+++ exercicios/aula12ex/script.js
@@ -2,7 +2,6 @@ var horario = window.document.getElementById('horario')
 var horas = new Date().getHours()
 var min = new Date().getMinutes()
 var sec = new Date().getSeconds()
-horario.innerHTML = `Horario atual ${horas}:${min}:${sec}`
 
 var imagem = window.document.getElementById('imagem')
 var fundo = window.document.querySelector('body')
@@ -15,3 +14,4 @@ if (horas >= 5 && horas < 12) {
     imagem.innerHTML = `<img src="imagens/noite.jpg" alt="imagem anoitecer">`
     fundo.style.backgroundColor = 'rgb(66, 57, 57)'
 }
+horario.innerHTML = `Horario atual ${horas}:${min}:${sec}`
