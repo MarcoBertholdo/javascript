2d3025c53ae5a74329c7ba1f80df656f88e4df5b
Marco Bertholdo <marquinhus_120@hotmail.com>
2023-12-31 16:07:29 -0300

    att fav

--- exercicios/aula12ex/script.js
+++ exercicios/aula12ex/script.js
@@ -8,12 +8,15 @@ function carregar() {
     if (horas >= 5 && horas < 12) {
         imagem.src = 'imagens/manha.jpg'
         document.body.style.backgroundColor = '#818996'
+        document.querySelector("link[rel='shortcut icon']").href = "sun.ico";
     } else if (horas >= 12 && horas < 19) {
         imagem.src = 'imagens/tarde.jpg'
         document.body.style.backgroundColor = '#f1d280'
+        document.querySelector("link[rel='shortcut icon']").href = "sun.ico";
     } else {
         imagem.src = 'imagens/noite.jpg'
         document.body.style.backgroundColor = '#3c3c3a'
+        document.querySelector("link[rel='shortcut icon']").href = "moon.ico";
     }
     horario.innerHTML = `Horario atual ${horas}:${min}:${sec}`
 }
