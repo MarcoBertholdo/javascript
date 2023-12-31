7097963460df55a54268f42a13848b0d8a4767aa
Marco Bertholdo <marquinhus_120@hotmail.com>
2023-12-31 17:49:52 -0300

    att idade

---
+++ exercicios/aula12ex/idade/script.js
@@ -0,0 +1,7 @@
+function checar() {
+    var img = window.document.getElementById('imagem')
+    var msg = window.document.getElementById('msg')
+    img.src = 'imagens/manha.jpg'
+    img.style.display = 'block'
+    msg.style.display = 'none'
+}
