d22ed7ee9c40f2dbcf87d8d7e55f8e41d53e42f4
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-01 17:49:17 -0300

    att

--- exercicios/aula12ex/idade/script.js
+++ exercicios/aula12ex/idade/script.js
@@ -1,7 +1,7 @@
 function checar() {
     var img = window.document.getElementById('imagem')
     var msg = window.document.getElementById('msg')
-    img.src = 'imagens/manha.jpg'
+    img.src = 'imagens/homem/adulto.png'
     img.style.display = 'block'
     msg.style.display = 'none'
 }
