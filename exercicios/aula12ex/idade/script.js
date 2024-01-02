ebad5d9231af68e372c0f4040ed6900bd3e1293d
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-02 05:02:38 -0300

    att

--- exercicios/aula12ex/idade/script.js
+++ exercicios/aula12ex/idade/script.js
@@ -3,40 +3,41 @@ function checar() {
     var idade = data.getFullYear()
     var ano = window.document.getElementById('ano')
     var sexo = window.document.getElementsByName('sexo')
-    var img = window.document.getElementById('imagem')
+    var foto = window.document.getElementById('foto')
     var msg = window.document.getElementById('msg')
     if (ano.value.lenght == 0 || Number(ano.value) > idade) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
     } else {
-     img.style.display = 'block'
-     msg.style.display = 'none'
      var anos = idade - Number(ano.value)
+     var genero = ''
+     var img = document.createElement('img')
+     img.setAttribute('id', 'imagem')
      if (sexo[0].checked) {
-        msg.style.display = 'block'
-        msg.innerHTML = `Detectamos um homem com <strong>${anos} anos</strong> de idade.`
+        genero = 'homem'
         document.body.style.backgroundColor = 'rgb(112, 112, 241)'
         if (anos <= 11) {
-            img.src = 'imagens/homem/bebe.png'
+            img.setAttribute('src', 'imagens/homem/bebe.png')
         } else if (anos > 11 && anos <= 20) {
-            img.src = 'imagens/homem/jovem.png'
+            img.setAttribute('src', 'imagens/homem/jovem.png')
         } else if (anos > 20 && anos <= 65) {
-            img.src = 'imagens/homem/adulto.png'
+            img.setAttribute('src', 'imagens/homem/adulto.png')
         } else {
-            img.src = 'imagens/homem/idoso.png'
+            img.setAttribute('src', 'imagens/homem/idoso.png')
         }
      } else {
-        msg.style.display = 'block'
-        msg.innerHTML = `Detectamos uma mulher com <strong>${anos} anos</strong> de idade.`
+        genero = 'mulher'
         document.body.style.backgroundColor = 'rgb(247, 175, 241)'
         if (anos <= 11) {
-            img.src = 'imagens/mulher/bebe.png'
+            img.setAttribute('src', 'imagens/mulher/bebe.png')
         } else if (anos > 11 && anos <= 20) {
-            img.src = 'imagens/mulher/jovem.png'
+            img.setAttribute('src', 'imagens/mulher/jovem.png')
         } else if (anos > 20 && anos <= 65) {
-            img.src = 'imagens/mulher/adulta.png'
+            img.setAttribute('src', 'imagens/mulher/adulta.png')
         } else {
-            img.src = 'imagens/mulher/idosa.png'
+            img.setAttribute('src', 'imagens/mulher/idosa.png')
         }
      }
     }
+    foto.innerHTML = `Detectamos um(a) ${genero} com <strong>${anos} anos</strong> de idade.`
+     foto.appendChild(img)
 }
