7128d3d29c128289264e29ac500cc13029119abc
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-04 17:45:16 -0300

    att v2

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -1,14 +1,18 @@
-var agora = new Date()
-var hora = agora.getHours()
-var min = agora.getMinutes()
-var sec = agora.getSeconds()
-console.log(`Agora são exatamente ${hora}:${min}:${sec} horas`)
-if (hora >= 5 && hora < 12) {
-    console.log('Bom dia')
-} else if (hora >= 12 && hora <= 18)
-        console.log('Boa tarde')
-        else if (hora > 18 && hora <= 24)
-        console.log('Boa Noite')
-        else {
-            console.log('Boa Madrugada')
-        }
+// Data de nascimento da pessoa (ano, mês - 1, dia)
+var dataNascimento = new Date(1996, 4, 14); // Mês é 0-based, então janeiro é 0
+
+// Data atual
+var dataAtual = new Date();
+
+// Calculando os dias vividos
+
+
+console.log("A pessoa viveu " + diasVividos + " dias.");
+
+
+
+console.log("A pessoa viveu aproximadamente " + mesesVividos + " meses.");
+
+
+
+console.log("A pessoa viveu aproximadamente " + semanasVividas + " semanas.");
--- exercicios/aula12ex/idade_v2/script.js
+++ exercicios/aula12ex/idade_v2/script.js
@@ -1,15 +1,20 @@
 function checar() {
     var data = new Date()
-    var idade = data.getFullYear()
+    var ano_atual = data.getFullYear()
     var ano = window.document.getElementById('ano')
     var mes = window.document.getElementById('mes')
     var dia = window.document.getElementById('dia')
     var sexo = window.document.getElementsByName('sexo')
     var foto = window.document.getElementById('foto')
-    if (ano.value === '' || Number(ano.value) > idade) {
+    var dataNascimento = new Date(Number(ano.value), Number(mes.value), Number(dia.value))
+    var diasVividos = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24))
+    var semanasVividas = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24 * 7))
+    var mesesVividos = (dataAtual.getFullYear() - dataNascimento.getFullYear()) * 12 + (dataAtual.getMonth() - dataNascimento.getMonth())
+
+    if (ano.value === '' || mes.value === '' || dia.value === '' || Number(ano.value) > ano_atual || Number(mes.value) < 1 || Number(mes.value) > 12 || Number(dia.value) < 1 || Number(dia.value > 31)) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
     } else {
-     var anos = idade - Number(ano.value)
+     var anos = ano_atual - Number(ano.value)
      var genero = ''
      var img = document.createElement('img')
      img.setAttribute('id', 'imagem')
@@ -38,8 +43,9 @@ function checar() {
             img.setAttribute('src', 'imagens/mulher/idosa.png')
         }
      }
-        foto.innerHTML = `Detectamos um(a) ${genero} com ${anos} anos de idade.<br>`
-        foto.innerHTML += `Essa pessoa nasceu em <strong>${dia.value}/${mes.value}/${ano.value}</strong>`
+        foto.innerHTML = `Detectamos um(a) ${genero} com <strong>${anos} anos</strong> de idade.<br>`
+        foto.innerHTML += `Essa já viveu:<br>`
+        foto.innerHTML += `${mesesVividos} meses<br>${semanasVividas} semanas<br>${diasVividos} dias`
         foto.appendChild(img)
     }
 }
