a2eb156213fc1e4a1d2cb4076174c20f9fed1956
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-11 04:13:42 -0300

    att

--- exercicios/aula14ex/numeros/script.js
+++ exercicios/aula14ex/numeros/script.js
@@ -1,22 +1,11 @@
-function carregar() {
-    var horario = window.document.getElementById('horario')
-    var horas = new Date().getHours()
-    var min = new Date().getMinutes()
-    var sec = new Date().getSeconds()
+function contar() {
+    var ini = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var pas = window.document.getElementById('passo')
+    var resultado = window.document.getElementById('resultado')
 
-    var imagem = window.document.getElementById('img')
-    if (horas >= 5 && horas < 12) {
-        imagem.src = 'imagens/manha.jpg'
-        document.body.style.backgroundColor = '#818996'
-        document.querySelector("link[rel='shortcut icon']").href = "sun.ico";
-    } else if (horas >= 12 && horas < 19) {
-        imagem.src = 'imagens/tarde.jpg'
-        document.body.style.backgroundColor = '#f1d280'
-        document.querySelector("link[rel='shortcut icon']").href = "sun.ico";
-    } else {
-        imagem.src = 'imagens/noite.jpg'
-        document.body.style.backgroundColor = '#3c3c3a'
-        document.querySelector("link[rel='shortcut icon']").href = "moon.ico";
+
+    for (var cont = ini.value;cont < fim.value;cont += pas.value) {
+        resultado.innerHTML += `${cont}`
     }
-    horario.innerHTML = `Horario atual ${horas}:${min}:${sec}`
 }
