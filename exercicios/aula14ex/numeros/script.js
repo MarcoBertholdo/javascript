4bdb8e1f37ee7755ea5baf4a6bc11ded74d2364d
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-12 10:54:17 -0300

    att contador final

--- exercicios/aula14ex/numeros/script.js
+++ exercicios/aula14ex/numeros/script.js
@@ -4,17 +4,22 @@ function contar() {
     var pas = window.document.getElementById('passo')
     var resultado = window.document.getElementById('resultado')
 
-    if (ini.value === '') {
-        window.alert('Insira um valor')
+    if (ini.value === '' || fim.value === '') {
+        window.alert('Insira um valor de INICIO e FIM.')
+    } else if (pas.value < 1 || Number(fim.value) < Number(pas.value)) {
+        pas.value = 1
+        window.alert('Passo inválido! O passo foi alterado automaticamente para 1.')
     } else {
-        if (resultado.lenght != 0) {
-            resultado.innerHTML = ``
-        }
-        for (var cont = Number(ini.value);cont <= Number(fim.value);cont += Number(pas.value)) {
-            resultado.innerHTML += `${cont} 👉 `
-        }
-        resultado.innerHTML += `<strong>FIM</strong> 🏁`
-    }
+          if (resultado.lenght != 0) {
+              resultado.innerHTML = ``
+          }
+          for (var cont = Number(ini.value);cont <= Number(fim.value);cont += Number(pas.value)) {
+              resultado.innerHTML += `${cont} 👉 `
+         }
+          resultado.innerHTML += `<strong>FIM</strong> 🏁`
+      }
+    
+    
 }
 
 function limpar() {
