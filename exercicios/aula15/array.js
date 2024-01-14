2c890533d57bca858a489856e8c5bba8ce6b89c1
Marco Bertholdo <marquinhus_120@hotmail.com>
2024-01-14 04:50:29 -0300

    att

--- exercicios/aula15/array.js
+++ exercicios/aula15/array.js
@@ -1,8 +1,14 @@
 let num = [2, 4, 7]
-num[3] = 5
-num.push(8)
+num[3] = 5 // adiciona o numero 5 na pos 3
+num.push(8) // adiciona o numero 8 no final da array
 
 console.log(num)
-console.log(num.length)
-console.log(num.sort())
-console.log(num.reverse())
+console.log(num.length) // quantidade de casas
+console.log(num.sort()) // coloca a array em ordem crescente
+console.log(num.reverse()) // coloca a array de trás pra frente
+let pos = num.indexOf(2)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(pos)
+}
--- exercicios/aula15/testes.js
+++ exercicios/aula15/testes.js
@@ -2,8 +2,18 @@ let num = [2, 4, 7]
 num[3] = 5
 num.push(8)
 
-let cont = 0
-while (cont <= num.length - 1) {
-    console.log(`Número ${num[cont]} na posição ${cont}`)
-    cont++
+for(let pos in num) {
+    console.log(`Número ${num[pos]} na posição ${pos}`)
 }
+
+/*
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`Número ${num[pos]} na posição ${pos}`)
+}*/
+
+
+/*let pos = 0
+while (pos <= num.length - 1) {
+    console.log(`Número ${num[pos]} na posição ${pos}`)
+    pos++
+}*/
