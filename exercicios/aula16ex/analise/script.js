d8ade1dcd5c0683ecafd5c0ca510cfb9bac3a235
MarcoBertholdo <marquinhus_120@hotmail.com>
2024-01-17 15:47:57 -0300

    att

--- exercicios/aula16ex/analise/script.js
+++ exercicios/aula16ex/analise/script.js
@@ -1,8 +1,9 @@
+let num = document.getElementById('numero')
+let res = document.getElementById('resultado')
+let tab = document.getElementById('tab')
+let numbers = []
 function adicionar() {
-    let num = document.getElementById('numero')
-    let res = document.getElementById('resultado')
-    let tab = document.getElementById('tab')
-    let numbers = [3, 5, 8, 10]
+
     if(num.value.length == 0 || Number(num.value) < 1 || Number(num.value) > 100) {
         window.alert('Valor inválido, insira um valor correto.')
     } else if(numbers.indexOf(Number(num.value)) == -1) {
@@ -10,13 +11,9 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         tab.appendChild(item)
-        if(numbers.length == 0) {
-            numbers[0] = Number(num.value)  
-        } else {
-            numbers[numbers.length] = Number(num.value)
-            res.innerHTML = `teste ${numbers}`
-            res.innerHTML += `<br>${numbers.indexOf(Number(num.value))}`
-        }
+        numbers.push(Number(num.value))
+        res.innerHTML = `teste ${numbers}`
+        res.innerHTML += `<br>${numbers.indexOf(Number(num.value))}`
     } else {
         window.alert(`O valor ${num.value} já foi adicionado.`)
     }
